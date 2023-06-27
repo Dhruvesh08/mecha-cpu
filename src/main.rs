1634c98738870086d2cebe5b3b8d0585e7793f0a
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-27 16:50:18 +0530

    mecha cpu temp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use cpu::cpu_temp;
+
+mod cpu;
 fn main() {
-    println!("Hello, world!");
+    let zone = 0; // Specify the thermal zone (0 or 1) for the CPU temperature you want to read
+
+    match cpu_temp(zone) {
+        Ok(temp) => println!("CPU temperature (zone {}): {:.2}°C", zone, temp),
+        Err(e) => eprintln!("Failed to read CPU temperature: {}", e),
+    }
 }
