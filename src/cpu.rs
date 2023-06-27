4330751e8f5bf905c15edf9909b5d0edb306e53a
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-27 16:56:02 +0530

    mecha cpu stress test added

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,4 @@
-use std::{fs, num::ParseFloatError, io};
+use std::{fs, num::ParseFloatError, io, process::Command};
 
 pub fn cpu_temp(zone: u8) -> Result<f64, io::Error> {
     let zone_path = format!("/sys/class/thermal/thermal_zone{}/temp", zone);
@@ -11,6 +11,23 @@ pub fn cpu_temp(zone: u8) -> Result<f64, io::Error> {
 
     let temp = temp / 1000.0; // Convert temperature from millidegrees Celsius to degrees Celsius
     Ok(temp)
+}
+
+pub fn stress_test_cpu(cpu_max_prime: u64) {
+    let output = Command::new("sysbench")
+        .arg("--test=cpu")
+        .arg("--num-threads=4")
+        .arg(format!("--cpu-max-prime={}", cpu_max_prime))
+        .arg("run")
+        .output()
+        .expect("Failed to execute sysbench command.");
+
+    if output.status.success() {
+        println!("Command executed successfully!");
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        println!("Command failed: {}", stderr);
+    }
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use cpu::cpu_temp;
+use cpu::{cpu_temp, stress_test_cpu};
 
 mod cpu;
 fn main() {
     let zone = 0; // Specify the thermal zone (0 or 1) for the CPU temperature you want to read
+
+    match cpu_temp(zone) {
+        Ok(temp) => println!("CPU temperature (zone {}): {:.2}°C", zone, temp),
+        Err(e) => eprintln!("Failed to read CPU temperature: {}", e),
+    }
+
+    let cpu_max_prime = 90000;
+    stress_test_cpu(cpu_max_prime);
 
     match cpu_temp(zone) {
         Ok(temp) => println!("CPU temperature (zone {}): {:.2}°C", zone, temp),
