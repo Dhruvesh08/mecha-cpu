1ff5877eb39e18e80e97fbd43676725bafaa2968
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-27 17:46:26 +0530

    mecha cpu stress test added

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,32 +1,41 @@
-use std::{fs, num::ParseFloatError, io, process::Command};
+use std::{fs, io, num::ParseFloatError, process::Command};
 
-pub fn cpu_temp(zone: u8) -> Result<f64, io::Error> {
-    let zone_path = format!("/sys/class/thermal/thermal_zone{}/temp", zone);
-    let temp_string = fs::read_to_string(zone_path)?;
-
-    let temp: f64 = temp_string
-        .trim()
-        .parse()
-        .map_err(|e: ParseFloatError| io::Error::new(io::ErrorKind::InvalidData, e))?;
-
-    let temp = temp / 1000.0; // Convert temperature from millidegrees Celsius to degrees Celsius
-    Ok(temp)
+pub trait CPUMonitor {
+    fn cpu_temp(&self, zone: u8) -> Result<f64, io::Error>;
+    fn stress_test_cpu(&self, cpu_max_prime: u64) -> Result<(), io::Error>;
 }
 
-pub fn stress_test_cpu(cpu_max_prime: u64) {
-    let output = Command::new("sysbench")
-        .arg("--test=cpu")
-        .arg("--num-threads=4")
-        .arg(format!("--cpu-max-prime={}", cpu_max_prime))
-        .arg("run")
-        .output()
-        .expect("Failed to execute sysbench command.");
+pub struct MyCPUMonitor;
 
-    if output.status.success() {
-        println!("Command executed successfully!");
-    } else {
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        println!("Command failed: {}", stderr);
+impl CPUMonitor for MyCPUMonitor {
+    fn cpu_temp(&self, zone: u8) -> Result<f64, io::Error> {
+        let zone_path = format!("/sys/class/thermal/thermal_zone{}/temp", zone);
+        let temp_string = fs::read_to_string(zone_path)?;
+
+        let temp: f64 = temp_string
+            .trim()
+            .parse()
+            .map_err(|e: ParseFloatError| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
+        let temp = temp / 1000.0; // Convert temperature from millidegrees Celsius to degrees Celsius
+        Ok(temp)
+    }
+
+    fn stress_test_cpu(&self, cpu_max_prime: u64) -> Result<(), io::Error> {
+        let output = Command::new("sysbench")
+            .arg("--test=cpu")
+            .arg("--num-threads=4")
+            .arg(format!("--cpu-max-prime={}", cpu_max_prime))
+            .arg("run")
+            .output()
+            .expect("Failed to execute sysbench command.");
+
+        if output.status.success() {
+            Ok(())
+        } else {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            Err(io::Error::new(io::ErrorKind::Other, format!("Command failed: {}", stderr)))
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use cpu::{cpu_temp, stress_test_cpu};
+use cpu::{MyCPUMonitor, CPUMonitor};
 
 mod cpu;
-fn main() {
-    let zone = 0; // Specify the thermal zone (0 or 1) for the CPU temperature you want to read
 
-    match cpu_temp(zone) {
+fn main() {
+    let monitor = MyCPUMonitor;
+
+    let zone = 0; // Specify the thermal zone (0 or 1) for the CPU temperature you want to read
+    match monitor.cpu_temp(zone) {
         Ok(temp) => println!("CPU temperature (zone {}): {:.2}°C", zone, temp),
         Err(e) => eprintln!("Failed to read CPU temperature: {}", e),
     }
 
     let cpu_max_prime = 90000;
-    stress_test_cpu(cpu_max_prime);
+    print!("Running stress test... ");
+    match monitor.stress_test_cpu(cpu_max_prime) {
+        Ok(()) => println!("Command executed successfully!"),
+        Err(e) => eprintln!("Failed to execute command: {}", e),
+    }
 
-    match cpu_temp(zone) {
+    match monitor.cpu_temp(zone) {
         Ok(temp) => println!("CPU temperature (zone {}): {:.2}°C", zone, temp),
         Err(e) => eprintln!("Failed to read CPU temperature: {}", e),
     }
